a1a0eb5c976f03e4725f1806eb89ab2b8c053ceb
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-04-14 16:20:56 -0400

    wrapper instead of contaienr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160414143442) do
 
   create_table "bootsy_image_galleries", force: :cascade do |t|
@@ -27,7 +26,6 @@ ActiveRecord::Schema.define(version: 20160414143442) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
