df4b216d37c51641f8a8cb596287877b61636bab
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-05-04 12:39:53 -0400

    redirect to current page after devise login + add admin link to nav if user is admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,19 @@ class ApplicationController < ActionController::Base
   before_filter :get_emails
   before_action :authenticate_user!
 
+  after_filter :store_location
+
+  #direct user to current location on sign in
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+
   def get_emails
     @mailing_list = MailingList.new
   end
