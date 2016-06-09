dc30f298acb2a2155811a71413f448c57e1c5f0f
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-06-09 11:51:33 -0400

    produ for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,11 @@ Rails.application.configure do
   }
 
   config.action_mailer.default_url_options = { :host => 'teacherslearningcode.herokuapp.com' }
-
+  Rails.application.routes.default_url_options[:host] =  'teacherslearningcode.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   # SMTP settings for mailgun
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
