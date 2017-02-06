3d5d36a47b46c7b9075860cb8d149b2c5dd1acb8
jrdnndtsch <jordanbdeutsch@gmail.com>
2017-02-06 15:08:59 -0500

    filtering on the drafts tab

--- app/controllers/lesson_dashboards_controller.rb
+++ app/controllers/lesson_dashboards_controller.rb
@@ -2,25 +2,33 @@ class LessonDashboardsController < ApplicationController
 	# respond_to :html, :js
 	def index
 		@lesson_bookmarked = current_user.find_up_voted_items
+
 		@lesson_drafts = current_user.lessons.has_been_submitted?(false)
-		# @lesson_submitted = current_user.lessons.is_submitted
+		@lesson_submitted = current_user.lessons.has_been_submitted?(true)
 
-		#create anonymous scope to start
-		@lessons = current_user.lessons.includes(:profile)
+		if params[:draft_filters]
+			if params[:lesson_title]
+				@lesson_drafts = params[:lesson_title]['Lesson Title'].present? && params[:lesson_title]['Lesson Title'] == '1' ? @lesson_drafts.order('title desc') : @lesson_drafts.order('title asc')
+			end
+			if params[:lesson_updated]
+				@lesson_drafts = params[:lesson_updated]['Updated'].present? && params[:lesson_updated]['Updated'] == '1' ? @lesson_drafts.order('updated_at desc') : @lesson_drafts.order('updated_at asc')
+			end
+		end
 
-		@lessons = @lessons.has_been_submitted?(params[:submitted] == 'true' ? true : false) if params[:submitted].present?
-
-		# @lessons = @lessons.order('title asc') if !params[:lesson_title].present?
-
-		# @lessons = @lessons.order('title desc') if params[:lesson_title].present?
-
-		@lessons = @lessons.order('updated_at desc') if params[:updated].present?
+		if params[:submitted_filters]
+			if params[:lesson_title]
+				@lesson_drafts = params[:lesson_title]['Lesson Title'].present? && params[:lesson_title]['Lesson Title'] == '1' ? @lesson_drafts.order('title desc') : @lesson_drafts.order('title asc')
+			end
+			if params[:lesson_updated]
+				@lesson_drafts = params[:lesson_updated]['Updated'].present? && params[:lesson_updated]['Updated'] == '1' ? @lesson_drafts.order('updated_at desc') : @lesson_drafts.order('updated_at asc')
+			end
+		end
 
 
 
 		#TODO ordering with acts as taggable
 
-		if params[:filter_options] || params[:submitted]
+		if params[:draft_filters]
 			respond_to do |format|
 				format.js { render :partial => "lesson_drafts_js" }
 			end
