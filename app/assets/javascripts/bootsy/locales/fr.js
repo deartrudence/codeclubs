5bf950b71a7b058b596cad64263398b77871bc88
trudy macnabb <trudy.macnabb@gmail.com>
2017-02-25 21:18:00 -0500

    bootsy bilingual

---
+++ app/assets/javascripts/bootsy/locales/fr.js
@@ -0,0 +1,54 @@
+/**
+ * English translations for bootstrap-wysihtml5 and Bootsy
+ */
+(function($){
+  $.fn.wysihtml5.locale['en'] = {
+    font_styles: {
+      normal: 'Texte normal',
+      h1: 'Titre 1',
+      h2: 'Titre 2',
+      h3: 'Titre 3'
+    },
+    emphasis: {
+      bold: 'Audacieux',
+      italic: 'Italic',
+      underline: 'Souligner',
+      small: 'Petite'
+    },
+    lists: {
+      unordered: 'Liste non ordonnée',
+      ordered: 'Liste ordonnée',
+      outdent: 'Outdent',
+      indent: 'Indent'
+    },
+    link: {
+      insert: 'Insert link',
+      cancel: 'Cancel',
+      target: 'Open link in new window'
+    },
+    image: {
+      insert: 'Insert image',
+      cancel: 'Cancel'
+    },
+    html: {
+      edit: 'Edit HTML'
+    },
+    colours: {
+      black: 'Noir',
+      silver: 'Argent',
+      gray: 'Gris',
+      maroon: 'Bordeaux',
+      red: 'Rouge',
+      purple: 'Violet',
+      green: 'Vert',
+      olive: 'Olive',
+      navy: 'Bleu Marin',
+      blue: 'Bleu',
+      orange: 'Orange'
+    },
+    bootsy: {
+      alertUnsaved: 'You have unsaved changes.',
+      error: 'Something went wrong. Please try again later.'
+    }
+  };
+}(jQuery));
